278ec1c6abfab45d5b95c5a05523b379642a2eab
yozi <j16412000kr@gmail.com>
2022-12-16 16:41:11 +0900

    prettier 정렬 부수기

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const numbers = [1,2,3,4,5];
 
-const reduceResult = numbers.reduce((acc, cur) => Number(acc) + Number(cur));
+const reduceResult = 
+numbers.reduce(
+(acc, cur) => Number(acc) + Number(cur)
+);
 
 console.log(reduceResult);
